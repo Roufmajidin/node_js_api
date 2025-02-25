863f79a69e59cc73c2aed781a74ab767cdddc312
Rouf Majid <roufmajidin777@gmail.com>
2025-02-26 06:38:56 +0700

    refactor migration migrate to laragon DB

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,30 @@
-const {DB_HOST,DB_USERNAME, DB_PASSWORD, DB_NAME} = process.env;
-var mysql = require('mysql');
+// const {DB_HOST,DB_USERNAME, DB_PASSWORD, DB_NAME} = process.env;
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//     host: DB_HOST,
+//     user: DB_USERNAME,
+//     password: DB_PASSWORD,
+//     database: DB_NAME
+// })
+// connection.connect(function(err, connection){
+//     if(err)throw err;
+//     console.log(DB_NAME + "Connected to the database");
+// })
+
+// module.exports = connection;
+const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
+var mysql = require('mysql2');
+
 var connection = mysql.createConnection({
     host: DB_HOST,
     user: DB_USERNAME,
     password: DB_PASSWORD,
     database: DB_NAME
-})
-connection.connect(function(err, connection){
-    if(err)throw err;
-    console.log(DB_NAME + "Connected to the database");
-})
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log(DB_NAME + " Connected to the database");
+});
 
 module.exports = connection;
